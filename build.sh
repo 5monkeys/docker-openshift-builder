cfcf4c3dd3c8870d909a17a86410505e196b45ee
Joakim Saario <saario.joakim@gmail.com>
2018-02-13 17:27:40 +0100

    Refactor build.sh and make shellcheck happy

--- build.sh
+++ build.sh
@@ -2,27 +2,50 @@
 
 set -x
 
-git config --global core.sshCommand "ssh -i $SOURCE_SECRET_PATH/ssh-privatekey -o StrictHostKeyChecking=no"
+configure_git_privkey() {
+    privatekey=$1
 
-BUILD_DIR=$(mktemp -d)
-git clone $SOURCE_REPOSITORY $BUILD_DIR && cd $BUILD_DIR
-if [ $? != 0 ]; then
-    exit 1
-fi
+    git config --global core.sshCommand \
+        "ssh -i $privatekey -o StrictHostKeyChecking=no"
+}
 
-export COMMIT=$(echo $BUILD | jq -r '.spec.revision.git.commit | select (. != null)')
-if [ -n "$COMMIT" ]; then
-	CHECKOUT=$COMMIT
-else
-	CHECKOUT=$SOURCE_REF
-fi
+configure_docker_credentials() {
+    cp "$PUSH_DOCKERCFG_PATH"/.dockercfg "$HOME"/.dockercfg
+}
 
-if [ -n "$CHECKOUT" ]; then
-    git checkout $CHECKOUT
-fi
+fast_checkout() {
+    repository=$1
+    ref=$2
 
-cp $PUSH_DOCKERCFG_PATH/.dockercfg $HOME/.dockercfg
+    git init
+    git remote add origin "$repository"
 
-export OUTPUT_REPOSITORY=$(echo $OUTPUT_IMAGE | cut -d: -f1)
+    [ -n "$ref" ] && fetch=$ref || fetch=HEAD
+    git fetch --depth 1 origin $fetch
+    git checkout FETCH_HEAD
+}
 
+get_git_ref() {
+    commit=$(echo "$BUILD" | jq -r '.spec.revision.git.commit |
+                                    select (. != null)')
+
+    if [ -n "$commit" ]; then
+        echo "$commit"
+    else
+        echo "$SOURCE_REF"
+    fi
+}
+
+cd "$(mktemp -d)" || exit 1
+
+git_ref=$(get_git_ref)
+configure_git_privkey "$SOURCE_SECRET_PATH"/ssh-privatekey
+fast_checkout "$SOURCE_REPOSITORY" "$git_ref"
+configure_docker_credentials
+
+COMMIT=$(git rev-parse HEAD)
+OUTPUT_REPOSITORY=$(echo "$OUTPUT_IMAGE" | cut -d: -f1)
+
+export COMMIT
+export OUTPUT_REPOSITORY
 bash -c "$BUILD_COMMAND"
